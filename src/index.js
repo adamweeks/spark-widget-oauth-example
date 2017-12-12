a7f5b2b66969aaebd94fd119634f4587ee18d401
Adam Weeks <adweeks@cisco.com>
2017-12-12 17:40:34 -0500

    chore: add linters and basic webpack config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = 'Cisco Spark Widgets & SDK';
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Spark Widget Oauth Example'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
