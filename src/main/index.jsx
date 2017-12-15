e649135b93c66c1a2796399f41e94fb32e477a65
Adam Weeks <adweeks@cisco.com>
2017-12-14 22:16:53 -0500

    feat(login): authorize and pass token

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Main = () =>
-  (
-    <div>
-      Spark OAuth Demo
-    </div>
-  );
+import Login from '../login';
+
+class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      authorized: false,
+      token: null
+    };
+
+    this.handleAuthorize = this.handleAuthorize.bind(this);
+  }
+
+  handleAuthorize(token) {
+    this.setState({
+      authorized: true,
+      token
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Spark OAuth Demo <br />
+        {
+          !this.state.authorized &&
+          (
+            <Login onAuthorization={this.handleAuthorize} />
+          )
+        }
+        {
+          this.state.authorized &&
+          (
+            <div>{this.state.token}</div>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 export default Main;
